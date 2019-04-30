40501c51859c59ae335da5469ac56b96086cdf13
BRS7 <bryutashea@gmail.com>
2019-04-30 12:47:07 -0700

    Rendering posts on index page

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
-const {ensureAuthenticated, checkLogin} = require('../config/auth');
+const Posts = require('../models/Posts');
+const { ensureAuthenticated, checkLogin } = require('../config/auth');
+const util = require('util');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const user = (req.user) ? req.user.name : "";
-  res.render('index',{
-    user
+
+  const posts = Posts.find({}, (err, posts) => {
+    const postsvar = posts;
+    res.render('index', {
+      user,
+      postsvar
+    });
   });
 });
 
-router.get('/dashboard', ensureAuthenticated, function(req, res, next) {
+router.get('/dashboard', ensureAuthenticated, function (req, res, next) {
   const user = (req.user) ? req.user.name : "";
   res.render('dashboard', {
     name: req.user.name,
